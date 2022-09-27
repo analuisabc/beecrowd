5f039c1b33685608561c82cf3b8580b10f871d39
Ana Luísa Chagas <114260644+analuisabc@users.noreply.github.com>
2022-09-27 16:34:38 -0300

    Add files via upload

---
+++ Python/1015.py
@@ -0,0 +1,11 @@
+import math
+x1, y1 = input().split()
+x2, y2 = input().split()
+x1 = float(x1)
+x2 = float(x2)
+y1 = float(y1)
+y2 = float(y2)
+x = math.pow((x2-x1), 2)
+y = math.pow((y2-y1), 2)
+d = math.pow((x+y), 1/2)
+print("{0:.4f}".format(d))
